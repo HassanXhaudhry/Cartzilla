8b224b52b4ce278ecc6e6afaa674331282c0dd76
Muhammad Hassan <hassanchaudhry39@gmail.com>
2024-06-18 18:28:01 +0500

    Updated

--- src/data/Products.js
+++ src/data/Products.js
@@ -45,7 +45,7 @@ const Products = [
     {
         id: 3,
         image: img3,
-        title: cart,
+        title: "Apple watch",
         price: "429.00",
         cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
