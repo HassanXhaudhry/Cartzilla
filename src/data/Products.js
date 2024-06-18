cdf22d77671b3e897105f762b730d8e3a8939234
Muhammad Hassan <hassanchaudhry39@gmail.com>
2024-06-18 18:14:58 +0500

    Updated

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,188 +1,230 @@
+import img1 from '../../src/assets/image1.png'
+import img2 from '../../src/assets/image2.png'
+import img3 from '../../src/assets/image3.png'
+import img4 from '../../src/assets/image4.png'
+import img5 from '../../src/assets/image5.png'
+import img6 from '../../src/assets/image6.png'
+import img7 from '../../src/assets/image7.png'
+import img8 from '../../src/assets/image8.png'
+import img9 from '../../src/assets/image9.png'
+import img10 from '../../src/assets/image10.png'
+import img11 from '../../src/assets/image11.png'
+import img12 from '../../src/assets/image12.png'
+import img13 from '../../src/assets/image13.png'
+import img14 from '../../src/assets/image14.png'
+import img15 from '../../src/assets/image15.png'
+import img16 from '../../src/assets/image16.png'
+import img17 from '../../src/assets/image17.png'
+import img18 from '../../src/assets/image18.png'
+import img19 from '../../src/assets/image19.png'
+import img20 from '../../src/assets/image20.png'
+import img21 from '../../src/assets/image21.png'
+import img22 from '../../src/assets/image22.png'
+import img23 from '../../src/assets/image23.png'
+import img24 from '../../src/assets/image24.png'
+import img25 from '../../src/assets/image25.png'
+import cart from '../../src/assets/cartIcon.png';
+
 const Products = [
     {
         id: 1,
-        image: "/src/assets/image1.png",
+        image: img1,
         title: "VRB01 Virtual Reality Glasses",
         price: "340.99",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 2,
-        image: "/src/assets/image2.png",
+        image: img2,
         title: "Apple iPhone 14 128GB White",
         price: "899.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 3,
-        image: "/src/assets/image3.png",
-        title: "Smart Watch Series 7, White",
+        image: img3,
+        title: cart,
         price: "429.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 4,
-        image: "/src/assets/image4.png",
+        image: img4,
         title: "Laptop Apple MacBook Pro 13 M2",
         price: "999.99",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 5,
-        image: "/src/assets/image5.png",
+        image: img5,
         title: "Tablet Apple iPad Air M1",
         price: "540.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 6,
-        image: "/src/assets/image6.png",
+        image: img6,
         title: "Headphones Apple AirPods 2 Pro",
         price: "224.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 7,
-        image: "/src/assets/image7.png",
+        image: img7,
         title: "Tablet Apple iPad Pro M1",
         price: "640.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 8,
-        image: "/src/assets/image8.png",
+        image: img8,
         title: "Wireless Bluetooth Headphones Sony ",
         price: "299.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 9,
-        image: "/src/assets/image9.png",
+        image: img9,
         title: "Xiaomi Wireless Buds Pro",
         price: "129.99",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 10,
-        image: "/src/assets/image10.png",
+        image: img10,
         title: "Smart Watch Series 7, White",
         price: "429.00",
-        cart: "/src/assets/cartIcon.png",
+        cart: cart,
         description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 11,
-        image: "/src/assets/image11.png",
+        image: img11,
         title: "VRB01 Camera Nikon Max",
         price: "652.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 12,
-        image: "/src/assets/image12.png",
+        image: img12,
         title: "Apple iPhone 14 128GB Blue",
         price: "899.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 13,
-        image: "/src/assets/image13.png",
+        image: img13,
         title: "Cotton lace blouse with necklace",
         price: "54.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 14,
-        image: "/src/assets/image14.png",
+        image: img14,
         title: "Leather sneakers with golden laces",
         price: "75.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 15,
-        image: "/src/assets/image15.png",
+        image: img15,
         title: "Leather handbag for women",
         price: "140.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 16,
-        image: "/src/assets/image16.png",
+        image: img16,
         title: "Polarized sunglasses for men",
         price: "96.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 17,
-        image: "/src/assets/image17.png",
+        image: img17,
         title: "Classic cotton men's shirt",
         price: "27.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 18,
-        image: "/src/assets/image18.png",
+        image: img18,
         title: "Denim midi skirt with pockets",
         price: "132.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 19,
-        image: "/src/assets/image19.png",
+        image: img19,
         title: "Knitted bag with a wooden handle",
         price: "105.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 20,
-        image: "/src/assets/image20.png",
+        image: img20,
         title: "Warm sweatshirts without a hoodie",
         price: "32.99",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 21,
-        image: "/src/assets/image21.png",
+        image: img21,
         title: "White sneakers with a massive sole",
         price: "86.50",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 22,
-        image: "/src/assets/image22.png",
+        image: img22,
         title: "Bed frame light gray 140x200 cm",
         price: "760.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 23,
-        image: "/src/assets/image23.png",
+        image: img23,
         title: "Blue armchair with iron legs ",
         price: "220.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 24,
-        image: "/src/assets/image24.png",
+        image: img24,
         title: "Loft-style lamp 120x80 cm",
         price: "140.00",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
     {
         id: 25,
-        image: "/src/assets/image25.png",
+        image: img25,
         title: "Green chair with wooden legs 60x100 cm",
         price: "320.50",
-        cart: "/src/assets/cartIcon.png"
+        cart: cart,
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.'
     },
 ];
 
